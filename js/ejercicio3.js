b8d8b55c89dc3c2727e2f5014e0b227efebcbc0c
guadavillarreal <guada.villarreal.gnv@gmail.comgit config --global user.emailgit config --global user.email guada.villarreal.gnv@gmail.com>
2023-11-04 19:00:54 -0300

    ejercicio 3 resolucion

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -4,19 +4,9 @@ deben mostrarse todas las cadenas concatenadas con un guión -.
 Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
 */
 
-// let texto=prompt("Ingrese un texto");
-// document.write(texto=texto+" - ");
-// texto=prompt("Ingrese un texto");
-// document.write(texto=texto+" - ");
-
-//confir (true); //lo inicializo en true 
 let texto;
-let texto2;
  do {
             texto = prompt("Ingrese el texto");
             texto=texto.concat(" - ");
-            //texto=" - "+texto+" - ";
             document.write(texto);
  } while (confirm("Seleccione OK para seguir ingresando cadenas de texto, caso contrario precione cancelar") == true);
- //document.write(texto.concat(" - "+ texto));
- //document.write(`<br>`+texto);
