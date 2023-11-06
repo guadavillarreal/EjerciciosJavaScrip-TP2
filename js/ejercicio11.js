508fe209357eb709d1455599e128958f7dfa012c
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-11-06 16:38:35 -0300

    ejercicio11

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -3,3 +3,17 @@ indique el nombre del mayor. *
 Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
  */
 
+let nombre;
+let edad ;
+
+
+
+// alert("Ingrese 3 nombres");
+// nombre=prompt("Ingrese un nombre");
+
+alert("Ingrese 3 edades");
+for (let index = 0; index <3; index++) {
+    edad=prompt("Ingrese la edad")
+    maxedad=Math.max(edad);
+    document.write(Math.max());
+}
