1a5db30e3bdc79ad254357ae7b3011bdb9509462
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-11-07 00:16:01 -0300

    resolucion del ejercicio 12

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -2,9 +2,12 @@
 indique el nombre del mayor.
 Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp */
 
-let nombre;
-let edad ;
-let posicion  ;
+let nombre1;
+let nombre2;
+let nombre3;
+let edad1;
+let edad2;
+let edad3;
 
 alert("Ingrese 3 nombres con sus edades");
 nombre1= prompt("Ingrese el 1er nombre");
@@ -16,10 +19,10 @@ edad3=prompt("ingrese la edad");
 
 const mayor = Math.max(edad1, edad2, edad3);
 if (mayor===edad1) {
-    document.write("El mayor es "+ nombre1 +" "+ edad1);
+    document.write("El mayor es "+ nombre1 +" de "+ edad1+ " años");
 }else if (mayor=== edad2) {
-    document.write("El mayor es "+ nombre2+" "+ edad2);
+    document.write("El mayor es "+ nombre2+" de "+ edad2+" años");
 } else {
-    document.write("El mayor es "+ nombre3+" "+ edad3);
+    document.write("El mayor es "+ nombre3+" de "+ edad3+ " años");
 }
 
---
+++ js/ejercicio12.js
@@ -0,0 +1,7 @@
+/*12- Realiza un script que genere un número aleatorio entre 1 y 99
+ */
+
+
+
+let numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+document.write(numeroAleatorio);
